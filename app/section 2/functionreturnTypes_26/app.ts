b59d25656a889ebd866a23a87393c5b1edd26e31
Abdulah Tilal <abdullahtilal26.com>
2023-02-12 01:48:27 +0500

    funstion return types section 2 lect 26

---
+++ app/section 2/functionreturnTypes_26/app.ts
@@ -0,0 +1,27 @@
+
+//here ts will infere the return type
+function add(n1:number,n2:number){
+    return n1+n2
+}
+//here explicitly add the return type
+function add2(n1:number,n2:number):number{
+    return n1+n2
+}
+
+//ts will infere the return type of this function as void
+//function printNumber(num:number):void{}
+function printNumber(num:number){
+    console.log(num)
+}
+
+//this will print undefine as your function doesnot return anything.
+console.log(printNumber(add(3,1))) 
+
+//if you set the return type as undefined,thank you have to put return statment and dont return anything example
+function printNumbers(num:number):undefined{
+    console.log(num)
+    return;
+}
+
+console.log(printNumbers(5))
+//better to use void instead of undefined
